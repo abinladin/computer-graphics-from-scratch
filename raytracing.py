bc2dd0b874d8744560fdf96e7e311249e3ffadc2
Abdullah Binladin <abdullahbinladin98@gmail.com>
2023-10-01 11:20:00 +0300

    added sphere and camera, best to switch to C

--- raytracing.py
+++ raytracing.py
@@ -1,11 +1,43 @@
 import tkinter as tk
 import logging
 
-WINDOW_WIDTH = 200
-WINDOW_HEIGHT = 200
-WINDOW_SCALE = 3
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+WINDOW_SCALE = 2
 
 log = logging.Logger("main", logging.INFO)
+
+def construct_sphere(x, y, z, r, color):
+    return {
+        "x": x,
+        "y": y,
+        "z": z,
+        "r": r,
+        "color": color,
+        "surface":
+        
+    }
+
+def construct_camera(x, y, z, ax, ay, az):
+    return {
+        "x": x,
+        "y": y,
+        "z": z,
+        "ax": ax,
+        "ay": ay,
+        "az": az
+    }
+
+def ray_trace(canvas, camera):
+    sphere1 = construct_sphere(0, 0, 0, 10, "#FFA0A0")
+    for x in range(int(-WINDOW_WIDTH/2), int(WINDOW_WIDTH/2)):
+        for y in range(int(-WINDOW_HEIGHT/2), int(WINDOW_HEIGHT/2)):
+
+            
+            
+            put_pixel(canvas, x, y, "")
+
+
 
 def viewport_to_canvas(x, y):
     return {"x": x * WINDOW_SCALE, "y": y * -WINDOW_SCALE, "d": 1}
@@ -19,7 +51,8 @@ def put_pixel(canvas: tk.Canvas, x: int, y: int, color: str):
         WINDOW_SCALE * WINDOW_HEIGHT / 2 + canvas_coordinates['y'] - scaled_pixel_width / 2,
         WINDOW_SCALE * WINDOW_WIDTH  / 2 + canvas_coordinates['x'] + scaled_pixel_width - scaled_pixel_width / 2,
         WINDOW_SCALE * WINDOW_HEIGHT / 2 + canvas_coordinates['y'] + scaled_pixel_width - scaled_pixel_width / 2 ,
-        fill=color
+        fill=color,
+        outline = color
     )
 
 
@@ -32,8 +65,9 @@ def draw_viewport(canvas: tk.Canvas):
 
 
 def render(canvas):
+    ray_trace(canvas, construct_camera(0, 0, 0, 0, 0, 1))
     draw_viewport(canvas)
-    put_pixel(canvas, 50,50, "#FFFFFF")
+
 
 if __name__ == "__main__":
     window = tk.Tk()

27620c137def4fcf98c2ca0ab138dbdf4b587c14
Abdullah Binladin <abdullahbinladin98@gmail.com>
2023-10-07 15:07:10 +0300

    implemented basic window loop

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
 #include<SDL2/SDL.h>
- int main(){
-    printf("Hello, world!");
-    return 0;
- }
+
+#define WIDTH 640
+#define HEIGHT 480
+#define SCALE 1
+
+
+int main(){
+   
+   SDL_Init(SDL_INIT_VIDEO);
+
+   SDL_Window* window;
+   window = SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 700, 500, SDL_WINDOW_RESIZABLE);
+   printf("Created window\n");
+
+   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+   printf("Created renderer\n");
+
+   SDL_Event e;
+   int quit = 0;
+   while (!quit){
+      while (SDL_PollEvent(&e)){
+         if (e.type == SDL_QUIT){
+               quit = 1;
+         }
+      }
+   }
+
+   printf("closing window...\n");
+   SDL_DestroyWindow(window);
+
+   SDL_Quit();
+}
